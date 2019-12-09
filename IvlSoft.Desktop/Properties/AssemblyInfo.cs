a48cdb59eaa67467261e0a489a5bbc64178060c0
Sriram Padmanabhan <sriram.p@sigtuple.com>
2019-12-09 20:11:35 +0530

    Changed AssemblyInfo to Alpha8
    Also uploader should always run as an administrator.

--- IvlSoft.Desktop/Properties/AssemblyInfo.cs
+++ IvlSoft.Desktop/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Software for operation of IntuCam-45 Fundus Camera")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sigtuple Technologies Pvt. Ltd.")]
-[assembly: AssemblyProduct("4.1.0.alpha7")]
+[assembly: AssemblyProduct("4.1.0.alpha8")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
