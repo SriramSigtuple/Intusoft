8f73b0c0152f1d6f9fd593dc8cf93a974f51865e
Kishore <kishore.raja@sigtuple.com>
2019-12-18 20:03:52 +0530

    Added ApproveModel and NotifyEmailModel in Cloud_Models.

---
+++ Cloud_Models/Models/MandaraModels/ApproveModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloud_Models.Models.MandaraModels
+{
+    public class ApproveModel: BaseCloudModel
+    {
+        public string analysis_id = string.Empty;
+        public string doctor_id = string.Empty;
+
+        public ApproveModel()
+        {
+            MethodType = HttpMethod.Post;
+        }
+    }
+}
---
+++ Cloud_Models/Models/MandaraModels/NotifyEmailModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloud_Models.Models.MandaraModels
+{
+    public class NotifyEmailModel: BaseCloudModel
+    {
+        public NotifyEmailModel()
+        {
+            MethodType = HttpMethod.Post;
+        }
+    }
+}
