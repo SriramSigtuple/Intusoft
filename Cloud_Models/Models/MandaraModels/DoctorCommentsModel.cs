9743b37a3468a2a39c98abd81f2aca2c2c02b9b7
Kishore <kishore.raja@sigtuple.com>
2019-12-20 19:03:31 +0530

    Added new cloud model DoctorCommentsModel.cs

---
+++ Cloud_Models/Models/MandaraModels/DoctorCommentsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloud_Models.Models.MandaraModels
+{
+    public class DoctorCommentsModel: BaseCloudModel
+    {
+
+        public DoctorCommentsModel()
+        {
+            MethodType = System.Net.Http.HttpMethod.Get;
+            URL_Model.API_URL_Start_Point = "aggrcomments";
+        }
+    }
+}
